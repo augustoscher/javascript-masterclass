9677041cec35143aac5676c54d7581fe89b4a66c
Augusto Scher <augustoscher@gmail.com>
2019-05-21 13:08:57 -0300

    ieee754 and numeric conversions

---
+++ ieee754_1.js
@@ -0,0 +1,50 @@
+console.log('ieee754');
+
+//arredondamentos
+let s = (0.1 + 0.2); 
+console.log(s);//0.30000000000000004
+s = (666.7 - 666.6);
+console.log(s); //0.10000000000002274
+s = (33.3 * 3);
+console.log(s); //99.89999999999999
+s = (12.2 / 0.1);
+console.log(s); //121.99999999999999
+
+console.log();
+
+//nao tem arredondamento
+s = (0.5 / 0.5);
+console.log(s); //1
+
+console.log();
+
+//others
+s = (1 / 0);
+console.log(s); //infinity (não da erro)
+console.log(Math.pow(10, 1000)); //infinity
+console.log(Number.MAX_VALUE * 2); //infinity
+console.log(Math.log(0)); //-infinity
+console.log(-Number.MAX_VALUE * 2); //-infinity
+
+console.log();
+
+//NaN
+//Not a Number é retornado quando realizamos operação numérica onde 
+//não é possível determinar o resultado
+console.log(10 * "a"); //NaN
+console.log(Math.sqrt(-9)); //raiz quadrada de -9 é NaN
+console.log(Math.log(-1)); //logaritmo de -9 é NaN
+console.log(parseFloat("javascript")); //NaN
+
+let test = (NaN === NaN);
+console.log(test);
+
+test = isNaN(parseFloat("javascript"));
+console.log(test)
+
+
+
+
+
+
+
---
+++ numeric_conversion.js
@@ -0,0 +1,38 @@
+console.log('numeric conversions');
+console.log(Number("10"));
+console.log(Number("9.9"));
+console.log(Number("0xFF"));
+console.log(Number("0b10"));
+console.log(Number("0o10"));
+console.log(Number());
+console.log(Number("Javascript"));
+
+console.log();
+console.log('numeric conversions - 2');
+console.log(~"10");
+console.log(~~"10");
+console.log(+"10");
+console.log("10" - 0);
+console.log("10" * 1);
+console.log("10" / 1);
+console.log("10" + 1); //vai concatenar
+console.log(+"10" + 1); //vai dar certo
+
+console.log();
+console.log('base conversion');
+console.log((0xA).toString(10));
+console.log((0b1010).toString(16));
+
+console.log();
+console.log('parseInt');
+console.log(parseInt("50")); //usar por padrão a base 10 (decimal)
+console.log(parseInt("A", 16));
+console.log(parseInt("A")); //nan pois ele nao sabe o que é "A"
+console.log(parseInt("9.9", 10)); //mesmo colocando na base decimal, ele perde precisao. vira 9.
+console.log(parseInt("9.9", 10)); 
+console.log(parseInt("11", 2)); //11 na base 2 é 3
+
+console.log();
+console.log('parseFloat');
+console.log(parseFloat("10")); 
+console.log(parseFloat("9.9")); //funciona pois é float
