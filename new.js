b0f67717c0e18d53860de12bc4d2697d3db2763a
Augusto Scher <augustoscher@gmail.com>
2019-05-23 02:24:47 -0300

    new, instanceof

--- new.js
+++ new.js
@@ -35,3 +35,27 @@ console.log();
 console.log(p3);
 console.log(p3.getAge());
 console.log(p3.__proto__ === p2.__proto__);
+
+console.log();
+console.log("=== instanceof === ");
+
+
+//diferença entre typeof e instanceof
+//typeof revela o tipo de dado de uma variável.
+const _instanceof = function (obj, fn) {
+  if (obj === fn.prototype) {
+    return true;
+  }
+  if (obj === null) {
+    return false;
+  }
+  return _instanceof(obj.__proto__, fn);
+}
+const date = new Date();
+console.log(date instanceof Date);//true
+console.log(date instanceof Object);//true
+console.log(date instanceof Array); //false
+console.log();
+console.log(_instanceof(date, Date));//true
+console.log(_instanceof(date, Object));//true
+console.log(_instanceof(date, Array)); //false
