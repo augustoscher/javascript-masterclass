51680b66724e13ed614941829dd58c05ee671850
Augusto Scher <augustoscher@gmail.com>
2019-05-30 16:59:19 -0300

    generators

--- generators/generator3.js
+++ generators/generator3.js
@@ -13,11 +13,38 @@ function sum(a, b) {
 }
 
 sum(19, 62).then(function (result) {
-  console.log(result);
+  // console.log(result);
 });
 
 sum(10, 9).then(result => {
+  // console.log(result);
+});
+
+// sum(127, 544).then(result => console.log(result));
+
+console.log();
+console.log("=== generators com promises === ");
+
+function async(fn) {
+  const gen = fn();
+  asyncR(gen);
+}
+
+function asyncR(gen, result) {
+  const obj = gen.next(result);
+  if (obj.done) {
+    return;
+  }
+  obj.value.then(function (result) {
+    asyncR(gen, result)
+  });
+}
+
+async(function* () {
+  const v1 = yield sum(2, 2);
+  const v2 = yield sum(4, 4);
+  const result = yield sum(v1, v2);
   console.log(result);
 });
 
-sum(127, 544).then(result => console.log(result));
+
