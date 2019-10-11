a1856b768b41a01bc8c64e6e02fae7b2adc68bbc
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-10-11 10:43:57 -0300

    arrays

--- test2.js
+++ test2.js
@@ -6,3 +6,19 @@ console.log(id.includes('#'));
 id = id.replace('#', '');
 
 console.log(id);
+console.log();
+console.log('-----')
+const cars = [
+  { name: "Toyoya Corolla", year: 2020 },
+  { name: "Peugeot 308", year: 2015 },
+	{ name: "Honda HR-v", year: 2019 },
+	{ name: "Citroen Cactus", year: 2018, default: true }
+];
+
+console.log(cars);
+console.log();
+const res = cars.filter(car => car.year > 2016);
+console.log(res);
+console.log();
+let defaultCar = cars.find(car => car.default )
+console.log(defaultCar)
