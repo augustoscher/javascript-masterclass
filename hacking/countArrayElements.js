cd863451a347bc198bbb5222ee8d2cdb33449a76
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-03-19 21:00:19 -0300

    hacking

---
+++ hacking/countArrayElements.js
@@ -0,0 +1,12 @@
+
+// Complete the birthday function below.
+//s: quares = [2, 2, 1, 3, 2, 4, 4]
+//d: day = 4
+//m: month = 2
+function birthday(s, d, m) {
+
+
+}
+
+birthday([2, 2, 1, 3, 2, 4, 4], 4, 2) //2
+birthday(4, 4, 1) //1
