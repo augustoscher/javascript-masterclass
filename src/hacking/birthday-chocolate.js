aedc7a5b50d3923eab4c46809a49cd26f00c7d33
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-03-19 21:38:06 -0300

    hacking

--- src/hacking/birthday-chocolate.js
+++ src/hacking/birthday-chocolate.js
@@ -3,10 +3,9 @@
 //s: quares = [2, 2, 1, 3, 2, 4, 4]
 //d: day = 4
 //m: month = 2
-function birthday(s, d, m) {
+const birthday = (s, d, m) => {
 
 
 }
 
-birthday([2, 2, 1, 3, 2, 4, 4], 4, 2) //2
-birthday(4, 4, 1) //1
+module.exports = birthday;
---
+++ src/test/birthday-chocolate.test.js
@@ -0,0 +1,15 @@
+const birthday = require('../hacking/birthday-chocolate');
+const assert = require('assert');
+
+describe('birthday-chocolate', function() {
+  it('Should return 1 item', () => {
+    const result = birthday(4, 4, 1); //1
+    assert.deepEqual(1, result);
+  });
+
+  it('Should return 2 items', () => {
+    const result = birthday([2, 2, 1, 3, 2, 4, 4], 4, 2) //2
+    assert.deepEqual(2, result);
+  });
+});
+
