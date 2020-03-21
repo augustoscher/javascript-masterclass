8977bcdb8afadd8c5a23c2ce2d48eaf155d9864e
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-03-21 20:55:26 -0300

    append and delete challenge

--- src/hacking/hacker-rank/append-delete-char.js
+++ src/hacking/hacker-rank/append-delete-char.js
@@ -1,60 +1,34 @@
-const deleteLastChat = s => s.slice(0, s.length - 1);
-const isEqual = (s1, s2) => s1 === s2;
-
-
 const equalsIgnroingSize = (s1, s2) => {
-  const a1 = Array.from(new Set(s1.split('')));
-  const a2 = Array.from(new Set(s2.split('')));
+  const a1 = Array.from(new Set(s1.split("")));
+  const a2 = Array.from(new Set(s2.split("")));
   if (a1.length === 1 && a2.length === 1) {
     return a1[0] === a2[0];
-  }
-  return false;
-}
-
-const isYes = (s, t, k) => {
-
-  if (equalsIgnroingSize(s, t)) {
-    let diference = Math.abs(t.length - s.length); //d2 k5
-    if (k < diference) {
-      return "No";
-    }
-    if (k == diference) {
-      return "Yes";
-    }
-    if (k > diference) {
-      return "Yes"
-    }
-    // return ((k - diference) % 2 == 0);
-  }
-
-  for (let i = 0; i < k; i++) {
-    if (!t.includes(s)) {
-      s = deleteLastChat(s);
-    } else {
-      const remainingSteps = k - i;
-      if (s.length + remainingSteps == k) {
-        return true;
-      }
-    }
   }
   return false;
 };
 
 function appendAndDelete(s, t, k) {
-  if (s === t && k == 0) {
-    return "Yes";
+  var end = 0;
+
+  for (var i = 0; i < Math.min(t.length, s.length); i++) {
+    if (s[i] == t[i]) {
+      end++;
+    } else {
+      break;
+    }
   }
 
-  if (k > s.length && (k-s.length == t.length)) {
-    return "Yes";
-  }
-  if (k > t.length && (k-t.length == s.length)){
-    return "Yes";
-  }
+  var maxIter = s.length + t.length;
+  var minIter = maxIter - 2 * end;
 
-  return isYes(s, t, k) ? "Yes" : "No";
+  if ((k >= minIter) & ((k - minIter) % 2 == 0)) {
+    return "Yes";
+  } else if (k >= maxIter) {
+    return "Yes";
+  } else {
+    return "No";
+  }
 }
-
 
 // console.log(appendAndDelete("zzzzz", "zzzzzzz", 4));
 // console.log(appendAndDelete("ashley", "ash", 2))
