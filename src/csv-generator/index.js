9a5be7515f5a95846173e87a8848a0b1851e23b4
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-04-30 11:01:24 -0300

    Csv Writer dependency

--- src/csv-generator/index.js
+++ src/csv-generator/index.js
@@ -1,14 +1,51 @@
-const Faker = require('faker');
+const Faker = require("faker");
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 
 const generateEmailAndLastname = size => {
-  console.log('email;lastname');
+  const data = [];
   for (let i = 0; i < size; i++) {
-    console.log(`${Faker.internet.exampleEmail()};${Faker.name.lastName()}`)
+    data.push({
+      email: `${Faker.internet.exampleEmail()}`,
+      lastname: `${Faker.name.lastName()}`
+    });
   }
+
+  const csvWriter = createCsvWriter({
+    path: "leads.csv",
+    header: [
+      {id: 'email', title: 'email'},
+      {id: 'lastname', title: 'lastname'},
+    ]
+  });
+
+  generateFile(csvWriter, data);
+}
+
+const generateEmail = (size) => {
+  const data = [];
+  for (let i = 0; i < size; i++) {
+    data.push({
+      email: `${Faker.internet.exampleEmail()}`
+    });
+  }
+
+  const csvWriter = createCsvWriter({
+    path: "leads_greater_than_25K.csv",
+    header: [{ id: "email", title: "Email" }],
+  });
+
+  generateFile(csvWriter, data);  
+};
+
+const generateFile = (writer, data) => {
+  writer
+  .writeRecords(data)
+  .then(() => console.log("The CSV file was written successfully"));
 }
 
 const main = () => {
-  generateEmailAndLastname(100);
+  // generateEmail(25_000);
+  generateEmailAndLastname(100)
 };
 
 main();
