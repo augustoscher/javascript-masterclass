92a0a7a9598f83552046df742e976a79b6f376af
Augusto S. Scher <augusto.scher@rdstation.com>
2021-04-04 15:13:41 -0300

    Big O complexity

---
+++ src/big-o-quadratic-time-complexity/index.js
@@ -0,0 +1,22 @@
+//Big O Quadratic Time Complexity
+
+// Sum all integers of an array:
+
+const nums = [1,2,3,4,5,6,7,8,9,10];
+
+// O(n) - Rate of growth scales in direct proportion to the input. 
+const sumHarder = arr => {
+   let sum = 0;
+   for (let i = 0; i < arr.length; i++) {
+       sum += arr[i];
+   }
+   return sum;
+}
+ 
+const result = sumHarder(nums);
+console.log('Sum: ', result)
+
+// O(1)	constant	fast
+const sumSmarter = arr => arr.length * (arr.length + 1) / 2;
+console.log('Sum: ', sumHarder(nums))
+
