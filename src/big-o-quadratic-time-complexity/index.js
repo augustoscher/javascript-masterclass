c522f69c1c344925e96893f5ba146f573a1298b4
Augusto S. Scher <augusto.scher@rdstation.com>
2021-04-05 09:57:37 -0300

    Bi g O complexity

--- src/big-o-quadratic-time-complexity/index.js
+++ src/big-o-quadratic-time-complexity/index.js
@@ -1,6 +1,6 @@
 //Big O Quadratic Time Complexity
 
-// Sum all integers of an array:
+// Sum all consecutives integers of an array:
 const nums = [1,2,3,4,5,6,7,8,9,10];
 
 // O(n) - Rate of growth scales in direct proportion to the input.
@@ -17,7 +17,7 @@ console.log('Sum: ', result);
 
 // O(1)	constant	fast
 // Regardless of the size of the input, our algorithm will perform the same, or, a constant number of operations.
-// Formula: n ( n + 1 ) / 2
+// Formula: n ( n + 1 ) / 2 - works only with consecutives integers
 const sumSmarter = arr => arr.length * (arr.length + 1) / 2;
 console.log('Sum: ', sumSmarter(nums));
 
