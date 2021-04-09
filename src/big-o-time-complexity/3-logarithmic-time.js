c5ff45b479d0e9c2ea6dd88d0113da0c2bfcab2f
Augusto S. Scher <augusto.scher@rdstation.com>
2021-04-09 17:08:29 -0300

    Big-O Logarithmic Time

---
+++ src/big-o-time-complexity/3-logarithmic-time.js
@@ -0,0 +1,3 @@
+// O(log(n)) — Logarithmic Time:
+
+// Logarithmic Time Complexity refers to an algorithm that runs in proportionally to the logarithm of the input size. Now, if you haven’t worked with Binary Search Trees, this may be a little difficult to conceptualize, so I’ll try and demonstrate it clearly below.
