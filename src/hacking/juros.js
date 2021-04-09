cbf9b09853410d4eea77c59da0263a1325b63d68
Augusto S. Scher <augusto.scher@rdstation.com>
2021-04-09 12:23:10 -0300

    Calculadora de juros

--- src/hacking/juros.js
+++ src/hacking/juros.js
@@ -1,19 +1,25 @@
 
-const calculate = (taxaAnual, valorBase, meses) => {
+const calculate = (taxaAnual, valorBase, meses, valorDepositoMes = 0) => {
   const percentual = (taxaAnual / 12).toFixed(2)
 
+  let valorOriginal = valorBase
   let totalJuros = 0
+  let totalDepositos = 0
   while (meses > 0) {
+    valorBase += valorDepositoMes
     const juros = (valorBase * percentual / 100)
     totalJuros += juros
+    totalDepositos += valorDepositoMes
     valorBase = valorBase + juros
     console.log(`Juros: ${juros.toFixed(2)} - Valor: ${valorBase.toFixed(2)}\n`)
     meses -= 1
   }
 
   console.log("----Totais----")
+  console.log(`Original: ${valorOriginal}`)
+  console.log(`Depositos: ${totalDepositos}`)
   console.log(`Juros: ${totalJuros}`)
   console.log(`Valor: ${valorBase}`)
 }
 
-calculate(2.66, 100000, 8)
+calculate(2.66, 27000, 12, 4000)
