df0aed15e994a6356a223c2ec43b50ff9f3ca49f
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-05-29 10:51:36 -0300

    feat: js clean code - functions - favor function programming

--- src/clean-code-js/functions.js
+++ src/clean-code-js/functions.js
@@ -171,3 +171,39 @@ class SuperArray extends Array {
     return this.filter(elem => !hash.has(elem));
   }
 }
+
+//========================================
+// Favor functional programming over imperative programming
+// JavaScript isn't a functional language in the way that Haskell is, but it has a functional flavor to it.
+// Functional languages can be cleaner and easier to test. Favor this style of programming when you can.
+
+const programmerOutput = [
+  {
+    name: "Uncle Bobby",
+    linesOfCode: 500
+  },
+  {
+    name: "Suzie Q",
+    linesOfCode: 1500
+  },
+  {
+    name: "Jimmy Gosling",
+    linesOfCode: 150
+  },
+  {
+    name: "Gracie Hopper",
+    linesOfCode: 1000
+  }
+];
+
+// BAD
+let totalOutput = 0;
+for (let i = 0; i < programmerOutput.length; i++) {
+  totalOutput += programmerOutput[i].linesOfCode;
+}
+
+// GOOD
+const totalOutput = programmerOutput.reduce(
+  (totalLines, output) => totalLines + output.linesOfCode,
+  0
+);
