7ddade67e2360900a564eeb0966cf3bab771e834
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-06-01 09:25:45 -0300

    feat: clean code JS

--- src/clean-code-js/functions.js
+++ src/clean-code-js/functions.js
@@ -268,3 +268,22 @@ class Cessna extends Airplane {
 }
 
 
+//========================================
+// Avoid type checking - pt1
+// JavaScript is untyped, which means your functions can take any type of argument. 
+// Sometimes you are bitten by this freedom and it becomes tempting to do type-checking in your functions.
+// There are many ways to avoid having to do this. The first thing to consider is consistent APIs.
+
+// BAD
+function travelToTexas(vehicle) {
+  if (vehicle instanceof Bicycle) {
+    vehicle.pedal(this.currentLocation, new Location("texas"));
+  } else if (vehicle instanceof Car) {
+    vehicle.drive(this.currentLocation, new Location("texas"));
+  }
+}
+
+// GOOD
+function travelToTexas(vehicle) {
+  vehicle.move(this.currentLocation, new Location("texas"));
+}
