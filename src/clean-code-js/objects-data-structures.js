785c668f093df7e9ad746183847d09692d6d8396
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-06-04 12:27:05 -0300

    feat: clean code js - object and data structures

--- src/clean-code-js/objects-data-structures.js
+++ src/clean-code-js/objects-data-structures.js
@@ -45,3 +45,111 @@ function makeBankAccount() {
 
 const account = makeBankAccount();
 account.setBalance(100);
+
+
+//========================================
+// 2. Make objects have private members
+// This can be accomplished through closures (for ES5 and below).
+
+// BAD
+const Employee = function(name) {
+  this.name = name;
+};
+
+Employee.prototype.getName = function getName() {
+  return this.name;
+};
+
+const employee = new Employee("John Doe");
+console.log(`Employee name: ${employee.getName()}`); // Employee name: John Doe
+delete employee.name;
+console.log(`Employee name: ${employee.getName()}`); // Employee name: undefined
+
+
+// GOOD
+function makeEmployee(name) {
+  return {
+    getName() {
+      return name;
+    }
+  };
+}
+
+const employee = makeEmployee("John Doe");
+console.log(`Employee name: ${employee.getName()}`); // Employee name: John Doe
+delete employee.name;
+console.log(`Employee name: ${employee.getName()}`); // Employee name: John Doe
+
+
+//========================================
+// 3. Prefer ES2015/ES6 classes over ES5 plain functions
+// It's very difficult to get readable class inheritance, construction, and method definitions for classical ES5 classes.
+// If you need inheritance (and be aware that you might not), then prefer ES2015/ES6 classes. However, prefer small functions 
+// over classes until you find yourself needing larger and more complex objects.
+
+// BAD
+const Animal = function(age) {
+  if (!(this instanceof Animal)) {
+    throw new Error("Instantiate Animal with `new`");
+  }
+
+  this.age = age;
+};
+
+Animal.prototype.move = function move() {};
+
+const Mammal = function(age, furColor) {
+  if (!(this instanceof Mammal)) {
+    throw new Error("Instantiate Mammal with `new`");
+  }
+
+  Animal.call(this, age);
+  this.furColor = furColor;
+};
+
+Mammal.prototype = Object.create(Animal.prototype);
+Mammal.prototype.constructor = Mammal;
+Mammal.prototype.liveBirth = function liveBirth() {};
+
+const Human = function(age, furColor, languageSpoken) {
+  if (!(this instanceof Human)) {
+    throw new Error("Instantiate Human with `new`");
+  }
+
+  Mammal.call(this, age, furColor);
+  this.languageSpoken = languageSpoken;
+};
+
+Human.prototype = Object.create(Mammal.prototype);
+Human.prototype.constructor = Human;
+Human.prototype.speak = function speak() {};
+
+// GOOD
+class Animal {
+  constructor(age) {
+    this.age = age;
+  }
+
+  move() {
+  }
+}
+
+class Mammal extends Animal {
+  constructor(age, furColor) {
+    super(age);
+    this.furColor = furColor;
+  }
+
+  liveBirth() {
+  }
+}
+
+class Human extends Mammal {
+  constructor(age, furColor, languageSpoken) {
+    super(age, furColor);
+    this.languageSpoken = languageSpoken;
+  }
+
+  speak() {
+  }
+}
