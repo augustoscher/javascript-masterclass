c2af8425b9d8349e35c555e64abfec6a7a7e18df
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-06-17 09:56:47 -0300

    feat: concurrency - Use Promises, not callbacks

---
+++ src/clean-code-js/concurrency.js
@@ -0,0 +1,41 @@
+//========================================
+// 1. Use Promises, not callbacks
+// Callbacks aren't clean, and they cause excessive amounts of nesting.
+// With ES2015/ES6, Promises are a built-in global type. Use them!
+
+// BAD
+import { get } from "request";
+import { writeFile } from "fs";
+
+get(
+  "https://en.wikipedia.org/wiki/Robert_Cecil_Martin",
+  (requestErr, response, body) => {
+    if (requestErr) {
+      console.error(requestErr);
+    } else {
+      writeFile("article.html", body, writeErr => {
+        if (writeErr) {
+          console.error(writeErr);
+        } else {
+          console.log("File written");
+        }
+      });
+    }
+  }
+);
+
+// GOOD
+import { get } from "request-promise";
+import { writeFile } from "fs-extra";
+
+get("https://en.wikipedia.org/wiki/Robert_Cecil_Martin")
+  .then(body => {
+    return writeFile("article.html", body);
+  })
+  .then(() => {
+    console.log("File written");
+  })
+  .catch(err => {
+    console.error(err);
+  });
+  
