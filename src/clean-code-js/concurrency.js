34a704bc59a0d80b1841dbc67776eb965383a7b4
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-06-17 10:02:29 -0300

    feat: concurrency - Async/Await are even cleaner than Promises

--- src/clean-code-js/concurrency.js
+++ src/clean-code-js/concurrency.js
@@ -38,4 +38,43 @@ get("https://en.wikipedia.org/wiki/Robert_Cecil_Martin")
   .catch(err => {
     console.error(err);
   });
-  
+
+
+//========================================
+// 2. Async/Await are even cleaner than Promises
+// Promises are a very clean alternative to callbacks, but ES2017/ES8 brings async and await which offer an even cleaner solution.
+// All you need is a function that is prefixed in an async keyword, and then you can write your logic imperatively without a then chain of functions.
+// Use this if you can take advantage of ES2017/ES8 features today!
+
+// BAD
+import { get } from "request-promise";
+import { writeFile } from "fs-extra";
+
+get("https://en.wikipedia.org/wiki/Robert_Cecil_Martin")
+  .then(body => {
+    return writeFile("article.html", body);
+  })
+  .then(() => {
+    console.log("File written");
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
+// GOOD
+import { get } from "request-promise";
+import { writeFile } from "fs-extra";
+
+async function getCleanCodeArticle() {
+  try {
+    const body = await get(
+      "https://en.wikipedia.org/wiki/Robert_Cecil_Martin"
+    );
+    await writeFile("article.html", body);
+    console.log("File written");
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+getCleanCodeArticle()
