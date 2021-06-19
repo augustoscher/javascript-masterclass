aa78959273922aa783a8c8ba192d16744f060e3b
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-06-18 22:54:40 -0300

    feat: clean code js - callers and callee

--- src/clean-code-js/objects-data-structures.js
+++ src/clean-code-js/objects-data-structures.js
@@ -269,3 +269,81 @@ class Employee {
     this.taxData = new EmployeeTaxData(ssn, salary);
   }
 }
+
+
+//========================================
+// 6. Function callers and callees should be close
+// If a function calls another, keep those functions vertically close in the source file.
+// Ideally, keep the caller right above the callee. We tend to read code from top-to-bottom, like a newspaper.
+// Because of this, make your code read that way.
+
+// BAD
+class PerformanceReview {
+  constructor(employee) {
+    this.employee = employee;
+  }
+
+  lookupPeers() {
+    return db.lookup(this.employee, "peers");
+  }
+
+  lookupManager() {
+    return db.lookup(this.employee, "manager");
+  }
+
+  getPeerReviews() {
+    const peers = this.lookupPeers();
+  }
+
+  perfReview() {
+    this.getPeerReviews();
+    this.getManagerReview();
+    this.getSelfReview();
+  }
+
+  getManagerReview() {
+    const manager = this.lookupManager();
+  }
+
+  getSelfReview() {
+  }
+}
+
+const review = new PerformanceReview(employee);
+review.perfReview();
+
+
+// GOOD
+class PerformanceReview {
+  constructor(employee) {
+    this.employee = employee;
+  }
+
+  perfReview() {
+    this.getPeerReviews();
+    this.getManagerReview();
+    this.getSelfReview();
+  }
+
+  getPeerReviews() {
+    const peers = this.lookupPeers();
+  }
+
+  lookupPeers() {
+    return db.lookup(this.employee, "peers");
+  }
+
+  getManagerReview() {
+    const manager = this.lookupManager();
+  }
+
+  lookupManager() {
+    return db.lookup(this.employee, "manager");
+  }
+
+  getSelfReview() {
+  }
+}
+
+const review = new PerformanceReview(employee);
+review.perfReview();
