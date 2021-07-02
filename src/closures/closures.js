58f3ae69273c87384f176d59c38835b5d93c6251
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-07-02 13:07:22 -0300

    feat: closures

--- src/closures/closures.js
+++ src/closures/closures.js
@@ -1,7 +1,6 @@
 // Closures are functions inside other functions that have access to their lexical environments.
 // The closure is a fundamental concept of JavaScript. This is a core concept in functional programming.
 
-
 function generator(a) {
   return function(param) {
     // a variable is visible inside inner function because is the same(scope) lexical environment.
@@ -17,3 +16,45 @@ add5(3); // returns 8 as a = 5 and param = 3
 add10(3); // returns 13 as a = 10 and param = 3
 
 console.log(add10(50)) //60
+
+// =====================
+
+// https://medium.com/@stephanowallace/javascript-mas-afinal-o-que-s%C3%A3o-closures-4d67863ca9fc
+// Closure é quando uma função é capaz de "lembrar" e acessar seu escopo léxico mesmo quando ela está sendo executada fora de seu escopo léxico.
+
+function pai(){
+  var x = 1;
+  function filho(){
+    console.log(x);
+    x++;
+  }
+  return filho;
+}
+
+var contador = pai();
+contador();    // 1
+contador();    // 2
+contador();    // 3
+
+// Modulos
+// Módulos são estruturas de código que fazem bom uso das closures, eg:
+function ModuloMatematico() {     
+  var x = 0;     
+  function somaUm() {
+      x++;        
+      console.log(x);     
+  }      
+  function subtraiUm() {         
+      x--;
+      console.log(x);     
+  }
+  return {         
+      somaUm: somaUm,         
+      subtraiUm: subtraiUm     
+  }; 
+}
+var teste = ModuloMatematico();  
+teste.somaUm();     // 1 
+teste.somaUm();     // 2
+teste.somaUm();     // 3
+teste.subtraiUm();  // 2
