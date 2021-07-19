2ea3ec94c630f819b8f2a53cf5b0f41d2a1fbff1
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-07-19 11:47:20 -0300

    feat: node16 features

--- src/node16/index.js
+++ src/node16/index.js
@@ -1,17 +1,6 @@
 // https://medium.com/the-node-js-collection/node-js-16-available-now-7f5099a97e70
+import regexpMatchIndices from './regexp.js';
 
-
-// 1. V8 upgraded to V8 9.0
-// ECMAScript RegExp Match Indices, which provide the start and end indices of the captured string.
-// The indices array is available via the .indices property on match objects when the regular expression has the /d flag.
-const regexpMatchIndices = () => {
-  const matchObj = /(Java)(Script)/d.exec('JavaScript'); 
-  console.log("Indices: ", matchObj.indices);
-  
-  console.log("Match: ", matchObj.indices[0]);
-  console.log("First Captured Group: ", matchObj.indices[1]);
-  console.log("Second Captured Group: ", matchObj.indices[2]);
-}
 
 const main = async () => {
   console.log("===== Node 16 =====\n")
