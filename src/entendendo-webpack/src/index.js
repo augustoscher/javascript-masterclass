7fd94da2a71fdf26dd52fc4e8e2f1b477d2bf357
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-08-10 00:37:33 -0300

    feat: understanding webpack

---
+++ src/entendendo-webpack/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM  from 'react-dom';
+
+const name = 'Augusto';
+
+ReactDOM.render(
+  <div>Nome: {name}</div>,
+  document.querySelector('#app')
+);
+
+console.log("oi");
---
+++ src/entendendo-webpack/webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'sixpack.js',
+    path: path.resolve(__dirname, 'lib')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html'
+    })
+  ]
+}
