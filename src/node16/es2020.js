20983aaddac21f07e7db5105698fa3547639fe4d
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-08-25 23:37:55 -0300

    feat: Nulish coalescing

--- src/node16/es2020.js
+++ src/node16/es2020.js
@@ -3,14 +3,14 @@
 // #3: Nullish Coalescing
 // Check if a variable is nullish – that is if it is either undefined or null,
 // like when it's okay for a variable to have an empty string, or even a false value.
-console.log(false ?? 'some truthy value');
 console.log(undefined ?? 'some truthy value');
 console.log(null ?? 'some truthy value');
+console.log(false ?? 'some truthy value');
 console.log(NaN ?? 'some truthy value');
 console.log();
 
 // compare to ||
-console.log(false || 'some truthy value');
 console.log(undefined || 'some truthy value');
 console.log(null || 'some truthy value');
+console.log(false || 'some truthy value');
 console.log(NaN || 'some truthy value');
