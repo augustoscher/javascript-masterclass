e8db51b8a73fa4b548013c34b883e14ee5c8d13b
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-09-01 20:49:21 -0300

    feat(es11): globalThis

--- src/node16/es2020.js
+++ src/node16/es2020.js
@@ -88,3 +88,15 @@ const str = 'abc';
 const iterator = str.matchAll(regexp);
 Array.from(iterator, result => console.log(result)); //prints all results
 
+
+// # 7: globalThis
+// If you wrote some cross-platform JS code which could run on Node, in the browser environment,
+// and also inside web-workers, you'd have a hard time getting hold of the global object.
+
+// This is because it is window for browsers, global for Node, and self for web workers.
+// If there are more runtimes, the global object will be different for them as well.
+
+// So you would have had to have your own implementation of detecting runtime and then using the correct global – that is, until now.
+// ES2020 brings us globalThis which always refers to the global object, no matter where you are executing your code:
+console.log(`\n= 7. globalThis = `)
+console.log(globalThis.setTimeout === global.setTimeout)
