ee069cbc9ed2970471ba44234c3e851e1f438c22
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-04-25 10:18:05 -0300

    feat: random number one-line

--- onliner.js
+++ onliner.js
@@ -1,10 +1,17 @@
-// one-liner usefull functions in js
+// One-Line usefull functions in js
 
-// FillRange
+// 1. FillRange
 // Returns array filled with given start and end positions
 const fillRange = (start, end) =>
   Array(end - start + 1)
     .fill()
     .map((_item, idx) => idx + start)
 
-console.log(fillRange(20, 22)) // [20, 21, 22]
+    console.log(fillRange(20, 22)) // [20, 21, 22]
+
+
+// 2. randomNumberInRange
+// Returns a random integer between given min and max
+const randomNumberInRange = (min = 0, max = 100) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+console.log(randomNumberInRange(2, 10)) // 4
