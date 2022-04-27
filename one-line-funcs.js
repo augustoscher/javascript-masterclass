7ce77f39314463dceefe1bff0f33fdec651d82da
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-04-27 00:43:06 -0300

    feat: removeDuplicates

--- one-line-funcs.js
+++ one-line-funcs.js
@@ -81,3 +81,7 @@ const randomBoolean = () => Math.random() >= 0.5;
 randomBoolean()
 
 
+// 14. removeDuplicates
+// remove all duplicated values
+const removeDuplicates = (arr) => [...new Set(arr)];
+removeDuplicates([31, 56, 12, 31, 45, 12, 31]); // [ 31, 56, 12, 45 ]
