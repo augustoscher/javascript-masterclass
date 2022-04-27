9676266b245374e69d542737ee0b0ea9ae42e3bd
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-04-26 23:49:22 -0300

    feat: randomHex

--- one-line-funcs.js
+++ one-line-funcs.js
@@ -14,18 +14,22 @@ fillRange(20, 22) // [20, 21, 22]
 // 2. randomNumberInRange
 // Returns a random integer between given min and max
 const randomNumberInRange = (min = 0, max = 100) => Math.floor(Math.random() * (max - min + 1)) + min;
-
 randomNumberInRange(2, 10) // 4
 
 
 // 3. rgbToHex
 // Converting the RGB to hex code
 const rgbToHex = (r, g, b) => "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-
 rgbToHex(0, 51, 255); // #0033ff
 
 
-// 4. Find the number of gap days between two dates
-const dayDiff = (date1, date2) => Math.ceil(Math.abs(date1.getTime() - date2.getTime()) / 86400000)
+// 4. randomHex
+// Generating randomHex color
+const randomHex = () => `#${Math.floor(Math.random() * 0xffffff).toString(16).padEnd(6, "0")}`;
+randomHex() // #a3c040
 
+
+// 5. dayDiff
+// Find the number of gap days between two dates
+const dayDiff = (date1, date2) => Math.ceil(Math.abs(date1.getTime() - date2.getTime()) / 86400000)
 dayDiff(new Date("2020-10-21"), new Date("2021-10-22")) // 366
