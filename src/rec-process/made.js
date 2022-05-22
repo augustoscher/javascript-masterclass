8a6e0065a6855851ca45ffdeaceffea765a1ccea
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-05-21 21:57:34 -0300

    feat: mode

--- src/rec-process/made.js
+++ src/rec-process/made.js
@@ -28,7 +28,9 @@ class NumberGenerator {
 const myNumberGenerator1 = new NumberGenerator("8, 1, 2, 5, 6, 8, 9, 5, 6, 17, 22, 55, 5")
 const myNumberGenerator2 = new NumberGenerator("1, 4, 10, 11, 10, 12, 16, 18, 22, 24, 122")
 const myNumberGenerator3 = new NumberGenerator("1, 500, 400, 500, 12, 1, 1, 122, 122, 122, 122")
+const myNumberGenerator4 = new NumberGenerator("1, 50, 50, 50, 1")
 
 console.log(myNumberGenerator1.mode()) // 5
 console.log(myNumberGenerator2.mode()) // 10
 console.log(myNumberGenerator3.mode()) // 10
+console.log(myNumberGenerator4.mode()) // 50
