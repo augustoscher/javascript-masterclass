e989d6985840d539a31cf2fa5edb7185e60adbb5
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-07-12 10:01:19 -0300

    feat: linked list

---
+++ src/algorithms/linked_list.js
@@ -0,0 +1,16 @@
+// https://github.com/trekhleb/javascript-algorithms/tree/master/src/data-structures/linked-list
+
+// Na ciência da computação, uma lista encadeada é uma coleção linear de elementos de dados, em que a ordem linear não é dada por sua locação física na memória.
+// Em vez disso, cada elemento aponta para o próximo. É uma estrutura de dados consistindo em um grupo de nós que juntos representam uma sequência.
+// Sob a forma mais simples, cada nó é composto de dados e uma referência (em outras palavras, uma ligação/conexão) para o próximo nó na sequência.
+// Esta estrutura permite inserção ou remoção eficiente de elementos de qualquer posição na sequência durante a iteração.
+
+// Variantes mais complexas adicionam ligações adicionais, permitindo uma inserção ou remoção mais eficiente a partir de referências de elementos arbitrárias.
+// Uma desvantagem das listas encadeadas é que o tempo de acesso é linear (e difícil de inserir em uma pipeline).
+// Acesso mais rápido, como acesso aleatório, não é viável. Arrays possuem uma melhor localização de cache em comparação com listas encadeadas (linked lists).
+
+// Complexidade:
+// | Acesso | Pesquisa | Inserção | Remoção |
+// | :----: | :------: | :------: | :-----: |
+// |  O(n)  |  O(n)    |  O(1)    |  O(n)   |
+
