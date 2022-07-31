46c3856d3572a0cc609fe23460227b8ce4347c83
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-07-31 11:13:18 -0300

    feat: es2022 features

---
+++ src/ecmascript/es2022.js
@@ -0,0 +1,14 @@
+// https://dev.to/jasmin/whats-new-in-es2022-1de6
+// Features of ES2022 - Node 18
+
+// 1. Method .at() of indexable values.
+// Datatypes supporting this function.
+
+// String
+// Array
+// All Typed Array classes: Uint8Array etc.
+
+[1, 2, 3, 4, 5].at(3) // returns 4
+[1, 2, 3, 4, 5].at(-2) // returns 4
+
+// -----------------------------------
