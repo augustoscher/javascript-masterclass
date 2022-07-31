65eca4b15e8f5dfb00020f173c5338e6a41e1d69
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-07-31 11:19:45 -0300

    feat: es2022 features

--- src/ecmascript/es2022.js
+++ src/ecmascript/es2022.js
@@ -12,3 +12,17 @@
 [1, 2, 3, 4, 5].at(-2) // returns 4
 
 // -----------------------------------
+
+// 2. RegExp match indices
+// Adding a flag /d to a regular expression produces match objects that records the start and end of each group capture.
+
+// There are different ways to match indices
+// a) Match indices for numbered group
+const regexp = /(a+)(b+)/d
+const matchObj = regexp.exec('aaaabb');
+console.log(matchObj);
+
+// Due to the regular expression flag /d, matchObj also has a property .indices 
+// that records for each numbered group where it was captured in the input string
+console.log(matchObj.indices); // [ [ 0, 6 ], [ 0, 4 ], [ 4, 6 ], groups: undefined ]
+// -----------------------------------
