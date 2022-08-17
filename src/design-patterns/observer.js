56f66fa909b5fd08b0d7d0e49de57e744bfcec6a
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-08-16 23:18:57 -0300

    feat: observable pattern

---
+++ src/design-patterns/observer.js
@@ -0,0 +1,22 @@
+// Observer Pattern
+// https://www.patterns.dev/posts/observer-pattern/
+// With the observer pattern, we can subscribe certain objects, the observers, to another object, called the observable.
+// Whenever an event occurs, the observable notifies all its observers!
+
+class Observable {
+  constructor() {
+    this.observers = [];
+  }
+
+  subscribe(func) {
+    this.observers.push(func);
+  }
+
+  unsubscribe(func) {
+    this.observers = this.observers.filter(observer => observer !== func);
+  }
+
+  notify(data) {
+    this.observers.forEach(observer => observer(data));
+  }
+}
