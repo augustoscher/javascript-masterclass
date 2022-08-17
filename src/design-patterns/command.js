d942099dd11b881ae6cd296e4ab9f5151d580c18
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-08-17 10:50:16 -0300

    feat: command pattern

--- src/design-patterns/command.js
+++ src/design-patterns/command.js
@@ -3,6 +3,13 @@
 // With the Command Pattern, we can decouple objects that execute a certain task from the object that calls the method.
 // patterns.dev/posts/command-pattern/
 
+// Pros
+// The command pattern allows us to decouple methods from the object that executes the operation.
+// It gives you more control if you're dealing with commands that have a certain lifespan, or commands that should be queued and executed at specific times.
+
+// Cons
+// The use cases for the command pattern are quite limited, and often adds unnecessary boilerplate to an application.
+
 class OrderManager {
   constructor() {
     this.orders = [];
