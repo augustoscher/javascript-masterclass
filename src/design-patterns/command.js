b52bb51d75d36c95276d4c72a3b554704262969d
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-08-17 10:19:13 -0300

    feat: command pattern

---
+++ src/design-patterns/command.js
@@ -0,0 +1,14 @@
+// Command Pattern
+// Decouple methods that execute tasks by sending commands to a commander
+// With the Command Pattern, we can decouple objects that execute a certain task from the object that calls the method.
+// patterns.dev/posts/command-pattern/
+
+class OrderManager {
+  constructor() {
+    this.orders = [];
+  }
+
+  execute(command, ...args) {
+    return command.execute(this.orders, ...args);
+  }
+}
