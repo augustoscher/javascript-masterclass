2e5b14ca9a8ff9637a4f37a12575aefc83896b3f
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-11-02 04:39:29 -0300

    feat: add LRU Cache implementation

---
+++ src/hacking/lru-cache.js
@@ -0,0 +1,4 @@
+// Least Recent Used Cache Implementation
+// A Least Recently Used (LRU) Cache organizes items in order of use, allowing you to quickly identify which item hasn't been used for the longest amount of time.
+// Under the hood, an LRU cache is often implemented by pairing a doubly linked list with a hash map.
+
