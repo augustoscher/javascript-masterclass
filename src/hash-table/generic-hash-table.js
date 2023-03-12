acec78351c53d26e35e4d2a50bc0a93a424f9947
Augusto S. Scher <augusto@jusbrasil.com.br>
2023-03-12 14:44:30 -0300

    feat: format code

--- src/hash-table/generic-hash-table.js
+++ src/hash-table/generic-hash-table.js
@@ -5,91 +5,90 @@
 
 class HashTableLinearP {
   constructor() {
-    this.table  = new Array(137);
-    this.values = new Array();
+    this.table = new Array(137)
+    this.values = new Array()
   }
 
   // Defining the hashing function which allows a sting to be used as a key
   hash(string) {
-    const H   = 37;
-    let total = 0;
+    const H = 37
+    let total = 0
 
     for (var i = 0; i < string.length; i++) {
-      total += H * total + string.charCodeAt(i);
+      total += H * total + string.charCodeAt(i)
     }
-    total %= this.table.length;
+    total %= this.table.length
     if (total < 1) {
-      this.table.length -1
+      this.table.length - 1
     }
-    return parseInt(total);
+    return parseInt(total)
   }
 
   put(key, data) {
-    let pos = this.hash(key);
-    if(this.table[pos] === undefined) {
-      this.table[pos]  = key;
-      this.values[pos] = data;
+    let pos = this.hash(key)
+    if (this.table[pos] === undefined) {
+      this.table[pos] = key
+      this.values[pos] = data
     } else {
-      while(this.table[pos] !== undefined) {
-        pos++;
+      while (this.table[pos] !== undefined) {
+        pos++
       }
-      this.table[pos]  = key;
-      this.values[pos] = data;
+      this.table[pos] = key
+      this.values[pos] = data
     }
   }
 
   get(key) {
-    const hash = this.hash(key);
+    const hash = this.hash(key)
     if (hash > -1) {
       for (let i = hash; this.table[i] !== undefined; i++) {
         if (this.table[i] === key) {
-          return this.values[i];
+          return this.values[i]
         }
       }
     }
-    return undefined;
+    return undefined
   }
 
   showDistro() {
     for (const key in this.table) {
-      if(this.table[key] !== undefined) {
-        console.log(key, ' : ', this.values[key]);
+      if (this.table[key] !== undefined) {
+        console.log(key, ' : ', this.values[key])
       }
     }
   }
 }
 
 function makeid() {
-  var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var text = ''
+  var possible =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 
   for (var i = 0; i < 10; i++)
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
+    text += possible.charAt(Math.floor(Math.random() * possible.length))
 
-  return text;
+  return text
 }
 
-(async function(){
+;(async function () {
   // ex1
   const hash = new HashTableLinearP()
   console.time('HashTableLinearP')
 
-  for (let i = 0; i < 100000; i++){
+  for (let i = 0; i < 100000; i++) {
     let v = makeid()
-    hash.put(v, { id: i+1, v });
+    hash.put(v, { id: i + 1, v })
   }
 
   console.timeEnd('HashTableLinearP')
-
 
   //ex2
   const m = new Map()
   console.time('Map')
 
-  for (let i = 0; i < 100000; i++){
+  for (let i = 0; i < 100000; i++) {
     let v = makeid()
-    m.set(v, { id: i+1, v });
+    m.set(v, { id: i + 1, v })
   }
   console.timeEnd('Map')
-
 })()
--- src/hash-table/hash-table-permutation.js
+++ src/hash-table/hash-table-permutation.js
@@ -5,29 +5,32 @@
 // Time Complexity of this solution : O(n) and  Space Complexity: O(1)
 
 function isPermutation(str1, str2) {
-    if (str1.length !== str2.length) {
-      return false;
-    }
-  
-    // generates array with zeros with 256 length
-    var hash_table_array = Array.apply(null, Array(256)).map(Number.prototype.valueOf, 0); 
+  if (str1.length !== str2.length) {
+    return false
+  }
 
-    // Increments with 1 the element in the position of chardCodeAt for each char of the string
-    for (var i = 0; i < str1.length; i++) {
-      hash_table_array[str1[i].charCodeAt(0)] += 1;
-    }
+  // generates array with zeros with 256 length
+  var hash_table_array = Array.apply(null, Array(256)).map(
+    Number.prototype.valueOf,
+    0
+  )
 
-    // decrements the correspondent position by chardCodeAt. If the value is less then zero, there isnt permutation because the char
-    // doesnt exists in the first string.
-    for (var i = 0; i < str2.length; i++) {
-      if ((hash_table_array[str2[i].charCodeAt(0)] -= 1) < 0) {
-        return false;
-      }
-    }
+  // Increments with 1 the element in the position of chardCodeAt for each char of the string
+  for (let i = 0; i < str1.length; i++) {
+    hash_table_array[str1[i].charCodeAt(0)] += 1
+  }
 
-    return true;
-  };
+  // decrements the correspondent position by chardCodeAt. If the value is less then zero, there isnt permutation because the char
+  // doesnt exists in the first string.
+  for (let i = 0; i < str2.length; i++) {
+    if ((hash_table_array[str2[i].charCodeAt(0)] -= 1) < 0) {
+      return false
+    }
+  }
+
+  return true
+}
 
 // console.log(isPermutation('abcd','dcba')) //true
 // console.log(isPermutation('abcd','dcbx')) //false
-console.log(isPermutation('augusto','otsugua')) //false
+console.log(isPermutation('augusto', 'otsugua')) //false
