ea0611b4933632cfa72d27562d8bb5b1c9b518a6
Augusto S. Scher <augusto@jusbrasil.com.br>
2023-03-11 22:23:31 -0300

    feat: format code

--- src/rec-process/made-v2.js
+++ src/rec-process/made-v2.js
@@ -1,8 +1,6 @@
-
 class NumberGenerator {
-
   constructor(numbers) {
-    this.numbersArray = numbers.split(", ");
+    this.numbersArray = numbers.split(', ')
   }
 
   mode() {
@@ -14,19 +12,25 @@ class NumberGenerator {
       // }
 
       // is the same:
-      const existingValue = map.get(element);
-      map.set(element, !!existingValue ? existingValue + 1 : 1)
-      return map;
+      const existingValue = map.get(element)
+      map.set(element, existingValue ? existingValue + 1 : 1)
+      return map
     }, new Map())
 
     return [...occurences.entries()].sort((a, b) => b[1] - a[1])[0][0]
   }
 }
 
-const myNumberGenerator1 = new NumberGenerator("8, 1, 2, 5, 6, 8, 9, 5, 6, 17, 22, 55, 5")
-const myNumberGenerator2 = new NumberGenerator("1, 4, 10, 11, 10, 12, 16, 18, 22, 24, 122")
-const myNumberGenerator3 = new NumberGenerator("1, 500, 400, 500, 12, 1, 1, 122, 122, 122, 122")
-const myNumberGenerator4 = new NumberGenerator("1, 50, 50, 50, 1")
+const myNumberGenerator1 = new NumberGenerator(
+  '8, 1, 2, 5, 6, 8, 9, 5, 6, 17, 22, 55, 5'
+)
+const myNumberGenerator2 = new NumberGenerator(
+  '1, 4, 10, 11, 10, 12, 16, 18, 22, 24, 122'
+)
+const myNumberGenerator3 = new NumberGenerator(
+  '1, 500, 400, 500, 12, 1, 1, 122, 122, 122, 122'
+)
+const myNumberGenerator4 = new NumberGenerator('1, 50, 50, 50, 1')
 
 console.log(myNumberGenerator1.mode()) // 5
 console.log(myNumberGenerator2.mode()) // 10
--- src/rec-process/made-v3.js
+++ src/rec-process/made-v3.js
@@ -1,24 +1,31 @@
-
 class NumberGenerator {
-
   constructor(numbers) {
-    this.numbersArray = numbers.split(", ");
+    this.numbersArray = numbers.split(', ')
   }
 
   mode() {
     const occurences = this.numbersArray.reduce((obj, item) => {
-      return { ...obj, [item]: (obj[item] || 0) + 1 };
+      return { ...obj, [item]: (obj[item] || 0) + 1 }
     }, {})
 
-    const mapped = Object.keys(occurences).map((key) => ({ k: key, v: occurences[key]}) )
+    const mapped = Object.keys(occurences).map((key) => ({
+      k: key,
+      v: occurences[key]
+    }))
     return mapped.sort((a, b) => b.v - a.v)[0].k
   }
 }
 
-const myNumberGenerator1 = new NumberGenerator("8, 1, 2, 5, 6, 8, 9, 5, 6, 17, 22, 55, 5")
-const myNumberGenerator2 = new NumberGenerator("1, 4, 10, 11, 10, 12, 16, 18, 22, 24, 122")
-const myNumberGenerator3 = new NumberGenerator("1, 500, 400, 500, 12, 1, 1, 122, 122, 122, 122")
-const myNumberGenerator4 = new NumberGenerator("1, 50, 50, 50, 1")
+const myNumberGenerator1 = new NumberGenerator(
+  '8, 1, 2, 5, 6, 8, 9, 5, 6, 17, 22, 55, 5'
+)
+const myNumberGenerator2 = new NumberGenerator(
+  '1, 4, 10, 11, 10, 12, 16, 18, 22, 24, 122'
+)
+const myNumberGenerator3 = new NumberGenerator(
+  '1, 500, 400, 500, 12, 1, 1, 122, 122, 122, 122'
+)
+const myNumberGenerator4 = new NumberGenerator('1, 50, 50, 50, 1')
 
 console.log(myNumberGenerator1.mode()) // 5
 console.log(myNumberGenerator2.mode()) // 10
