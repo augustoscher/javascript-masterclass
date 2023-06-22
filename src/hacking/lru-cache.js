100ec40cab348b4799719907668c7b40115280ca
Augusto S. Scher <augusto@jusbrasil.com.br>
2023-06-22 16:08:45 -0300

    feat: update LRU

--- src/hacking/lru-cache.js
+++ src/hacking/lru-cache.js
@@ -4,28 +4,28 @@
 
 class LRUCache {
   constructor(capacity) {
-    this.capacity = capacity;
-    this.cache = new Map();
+    this.capacity = capacity
+    this.cache = new Map()
   }
 
   set(key, value) {
     //if we're at capacity, we need to remove the least-recently-used item:
-    if(this.cache.size >= this.capacity) {
-      const leastRecentlyUsedKey = this.cache.keys().next().value;
+    if (this.cache.size >= this.capacity) {
+      const leastRecentlyUsedKey = this.cache.keys().next().value
       this.cache.delete(leastRecentlyUsedKey)
     }
     // even so, we need to reinsert the value in the cache to make it more recently used.
-    this.cache.delete(key);
-    this.cache.set(key, value);
+    this.cache.delete(key)
+    this.cache.set(key, value)
   }
 
   get(key) {
     if (this.cache.has(key)) {
       // we need to make it more recently used
-      const value = this.cache.get(key);
-      this.cache.delete(key);
-      this.cache.set(key, value);
-      return value;
+      const value = this.cache.get(key)
+      this.cache.delete(key)
+      this.cache.set(key, value)
+      return value
     }
   }
 
@@ -34,22 +34,22 @@ class LRUCache {
   }
 }
 
-const cache = new LRUCache(5);
-cache.set('a', 1);
-cache.set('b', 2);
-cache.set('c', 3);
-cache.print(); // a is the least-recently used
+const cache = new LRUCache(5)
+cache.set('a', 1)
+cache.set('b', 2)
+cache.set('c', 3)
+cache.print() // a is the least-recently used
 
-console.log(cache.get("b"));
-cache.print(); // /b is the more recently used
+console.log(cache.get('b'))
+cache.print() // /b is the more recently used
 
-cache.set("a", 111); // a is the more recent used
-cache.print();
+cache.set('a', 111) // a is the more recent used
+cache.print()
 
-cache.set('d', 4);
-cache.set('e', 5);
-cache.print();
+cache.set('d', 4)
+cache.set('e', 5)
+cache.print()
 
 // c is the least-recently used, so if we add a new value, it should be removed:
-cache.set('f', 6);
-cache.print();
+cache.set('f', 6)
+cache.print()
--- src/hash-table/hash-table-trip.js
+++ src/hash-table/hash-table-trip.js
@@ -1,10 +1,9 @@
 // HashTable/HashMap Examples
 // https://javascript.plainenglish.io/algorithm-in-javascript-hash-table-7b0464d2b81b
-// You are on a biz trip and travelling from one city to another. 
-// you have a stack of unsorted flight boarding passes. 
+// You are on a biz trip and travelling from one city to another.
+// you have a stack of unsorted flight boarding passes.
 // only departure city and destination city are on the boarding pass.
 // how do you find the first departure city and your final destination city, write the solution in javascript
-
 
 // Key solution strategy — First note, that each city will be shown up in the boarding ticket 2 times (one for departure and one for arrival) — Except the first city (being the first departure) and the last city (being the last arrival) which will show up only once.
 // So arithmatically all cities will cancel each other, except the first departure and final arrival city.
@@ -13,50 +12,42 @@
 //sp - sal
 //nav - sp
 
-const findDepartureArrival = tickets => {
+const findDepartureArrival = (tickets) => {
   console.time('ObjectSolution')
 
   const hashMap = {}
 
   for (let tkt in tickets) {
-    const depart = tickets[tkt].departure;
-    const arrival = tickets[tkt].arrival;
+    const depart = tickets[tkt].departure
+    const arrival = tickets[tkt].arrival
 
-    if (!(depart in hashMap))
-      hashMap[depart] = -1;
-    else
-      hashMap[depart] = hashMap[depart] -1;
+    if (!(depart in hashMap)) hashMap[depart] = -1
+    else hashMap[depart] = hashMap[depart] - 1
 
-    if (!(arrival in hashMap))
-      hashMap[arrival] = 1;
-    else
-      hashMap[arrival] = hashMap[arrival] + 1;
+    if (!(arrival in hashMap)) hashMap[arrival] = 1
+    else hashMap[arrival] = hashMap[arrival] + 1
   }
 
   console.log(hashMap)
   console.timeEnd('ObjectSolution')
 }
 
-const findArrivalWithMap = tickets => {
+const findArrivalWithMap = (tickets) => {
   console.time('MapSolution')
 
   const m = new Map()
 
   for (let tkt in tickets) {
-    const depart = tickets[tkt].departure;
-    const arrival = tickets[tkt].arrival;
+    const depart = tickets[tkt].departure
+    const arrival = tickets[tkt].arrival
 
     let mapValue = m.get(depart)
-    if (mapValue)
-      m.set(depart, mapValue -1)
-    else
-      m.set(depart, -1)
+    if (mapValue) m.set(depart, mapValue - 1)
+    else m.set(depart, -1)
 
     mapValue = m.get(arrival)
-    if (mapValue)
-      m.set(arrival, mapValue + 1)
-    else 
-      m.set(arrival, 1)
+    if (mapValue) m.set(arrival, mapValue + 1)
+    else m.set(arrival, 1)
   }
 
   console.log(m)
@@ -70,20 +61,20 @@ const ticketsMap = {
     arrival: 'San Francisco'
   },
   tkt2: {
-      departure: 'San Francisco',
-      arrival: 'New York'
+    departure: 'San Francisco',
+    arrival: 'New York'
   },
   tkt3: {
-      departure: 'Moscow',
-      arrival: 'Mali'
+    departure: 'Moscow',
+    arrival: 'Mali'
   },
   tkt4: {
-      departure: 'Barcelona',
-      arrival: 'Moscow'
+    departure: 'Barcelona',
+    arrival: 'Moscow'
   },
   tkt5: {
-      departure: 'New York',
-      arrival: 'Barcelona'
+    departure: 'New York',
+    arrival: 'Barcelona'
   }
 }
 
